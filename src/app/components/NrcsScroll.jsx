6ed068e517c04bd2fcda504868137fbacc758234
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-09-18 23:37:38 +0530

    Scroll completes

--- src/app/components/NrcsScroll.jsx
+++ src/app/components/NrcsScroll.jsx
@@ -1,14 +1,160 @@
-import React, { useState } from 'react'
+
+'use client'
+
+import React, { useState, useRef, useEffect, useCallback } from 'react'
+import Timer from './Timer';
+import { addressmysql } from "../lib/common";
+import Script from "../Script";
+
+
+const vTrailingSpace = 0.1;
 
 const NrcsScroll = () => {
+    const [slugs, setSlugs] = useState([]);
+    const [currentSlug, setCurrentSlug] = useState(-1);
+    const [ScriptID, setScriptID] = useState("");
+    const [currentSlugSlugName, setCurrentSlugSlugName] = useState("");
+
+
+    const [runOrderTitles, setRunOrderTitles] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(() => {
+        const today = new Date();
+        const yyyy = today.getFullYear();
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const dd = String(today.getDate()).padStart(2, '0');
+        return `${yyyy}-${mm}-${dd}`;
+    });
+    const [selectedRunOrderTitle, setSelectedRunOrderTitle] = useState("0600 Hrs");
+    const [horizontalSpeed, setHorizontalSpeed] = useState(0.01);
+    const [tickerRunning, setTickerRunning] = useState(false);
+    const [scrollData, setScrollData] = useState([]);
 
     const [NrcsBreakingText, setNrcsBreakingText] = useState(true)
+    const indexRefTicker = useRef(1);
+
+    const handleSelectionChange = (e) => {
+        setSelectedRunOrderTitle(e.target.value);
+
+    };
+
+    const handleDateChange = (event) => {
+        const date = event.target.value;
+        setSelectedDate(date)
+    };
+
+    async function fetchNewsID() {
+        try {
+            const res = await fetch(addressmysql() + "/getNewsIDforCG");
+            const data = await res.json();
+            setRunOrderTitles(data.data);
+        } catch (error) {
+            // console.error('Error fetching RunOrderTitles:', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchNewsID();
+    }, []);
+
+    const fetchRO = useCallback(async () => {
+        if (selectedRunOrderTitle === "") {
+            return;
+        }
+        try {
+            const res = await fetch(
+                addressmysql() + `/ShowRunOrder?NewsId=${selectedRunOrderTitle}&date=${selectedDate}`
+            );
+            const data = await res.json();
+            setSlugs(data.data);
+        } catch (error) {
+            // console.error('Error fetching data:', error);
+        }
+    }, [selectedRunOrderTitle, selectedDate]);
+
+
+
+
+    useEffect(() => {
+        fetchRO();
+    }, [selectedRunOrderTitle, fetchRO]);
 
     const handleChange = (event) => {
         const value = event.target.value === 'true';  // Convert string to boolean
         setNrcsBreakingText(value)
     };
 
+    const playticker = async () => {
+        let scripts = [];
+
+        try {
+            const res = await fetch(addressmysql() + "/getScrollData", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    bulletinname: "Scroll", bulletindate: selectedDate
+                }),
+            });
+            const result = await res.json()
+            scripts = result.data.map(row => row.Script);
+            console.log(scripts)
+            if (scripts != []) {
+                setTickerRunning(true);
+                setScrollData(scripts);
+                indexRefTicker.current = 1;
+                const exportValues = {
+                    vSpeed: `${horizontalSpeed}`,
+                    vStart: true,
+                    vStackCount: "1",
+                    // vStackSize: 1,
+                    vReset: true,
+                    tText: '',
+                    tScroll: `{ 'Group1': [{ 'vLeadingSpace':'0', 'vTrailingSpace':'${vTrailingSpace}', 'tText': '${scripts[0]}' }] }`,
+                }
+                fetch("/api/playwithexportedvalues", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        project: '25IN_ChannelPackaging_351.450',
+                        scene: 'vimlesh_ticker',
+                        timeline: 'In',
+                        slot: "1",
+                        exportedvalues: Object.entries(exportValues).map(([name, value]) => ({ name, value }))
+                    })
+                })
+
+
+            }
+
+        } catch (error) {
+            // console.error('Error saving content:', error);
+        }
+
+
+
+    }
+    const onhorizontalSpeedChange = async (e) => {
+        setHorizontalSpeed(e.target.value);
+        const res = await fetch("/api/sendCommand", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ command: `SCENE "25IN_ChannelPackaging_351.450/vimlesh_ticker" Export "vSpeed" SetValue "${e.target.value}"` })
+        })
+    }
+
+    const onStopTicker = async () => {
+        fetch("/api/timeline", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ project: '25IN_ChannelPackaging_351.450', scene: 'vimlesh_ticker', timeline: "Out" })
+        })
+        setTickerRunning(false);
+
+    }
+    useEffect(() => {
+        fetchRO();
+    }, [selectedRunOrderTitle, fetchRO]);
 
     return (<>
         <div>
@@ -16,88 +162,196 @@ const NrcsScroll = () => {
                 Nrcs Scroll
             </h1>
         </div>
+
+
         <div>
-            <table style={{ borderCollapse: 'collapse', width: 400 }}>
-                <thead>
-                    <tr>
-                        <th style={{ border: '1px solid black', padding: '8px', textAlign: 'left' }}>Feature</th>
-                        <th style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Date and Time</td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
+            <label htmlFor="date-selector">Select a date: </label>
+            <input
+                id="date-selector"
+                type="date"
+                value={selectedDate}
+                onChange={handleDateChange}
+            />
 
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Scroll</td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
+        </div>
+        <div>
+            Run Orders:
+            <select
+                value={selectedRunOrderTitle}
+                onChange={handleSelectionChange}
+            >
+                <option value="" disabled>
+                    Select a Run Order
+                </option>
+                {runOrderTitles &&
+                    runOrderTitles?.map((runOrderTitle, i) => (
+                        <option key={i} value={runOrderTitle.title}>
+                            {runOrderTitle.title}
+                        </option>
+                    ))}
+            </select>
+            {slugs?.length} slugs
+        </div>
 
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Lower Third
-                            Breaking News</td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
 
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>  Full Pag
-                            Breaking News</td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>News Update</td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}> Two Liner
-                            Header:
-                            <br />
-                            Header: <br />
-                            <label>
-
+        <div style={{ display: 'flex' }}>
+            <div style={{ height: 450, width: 400, overflow: "auto", border: '1px solid red' }}>
+                {slugs &&
+                    slugs?.map((val, i) => (<div
+                        title={val.ScriptID}
+                        onClick={() => {
+                            setScriptID(val.ScriptID);
+                            setCurrentSlug(i);
+                            setCurrentSlugSlugName(val.SlugName);
+                        }}
+                        key={i} style={{
+                            display: 'flex', color: currentSlug === i ? "white" : "black",
+                            backgroundColor: currentSlug === i ? "green" : "#E7DBD8",
+                            margin: 10,
+                        }}>
+                        <div
+                            style={{
+                                minWidth: 320,
+                                maxWidth: 320,
+                            }}
+                        >
+                            {i + 1}{" "}
+                            <label style={{ cursor: "pointer" }}>{val.SlugName}</label>{" "}
+                        </div>
+                        <div>{val.MediaInsert}</div>
+                    </div>
+                    ))}
+            </div>
+            <div style={{ height: 350, }}>
+                <Script
+                    ScriptID={ScriptID}
+                    title={selectedRunOrderTitle}
+                    currentSlugSlugName={currentSlugSlugName}
+                />
+            </div>
+            <div style={{ height: 450, }}>
+                <table style={{ borderCollapse: 'collapse', width: 400 }}>
+                    <thead>
+                        <tr>
+                            <th style={{ border: '1px solid black', padding: '8px', textAlign: 'left' }}>Feature</th>
+                            <th style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Date and Time</td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
+                                <button>Play</button>
+                                <button>Stop</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Scroll
+                                Speed:
                                 <input
-                                    type="radio"
-                                    value={true}
-                                    checked={NrcsBreakingText === true}
-                                    onChange={handleChange}
+                                    style={{ width: "60px" }}
+                                    onChange={(e) => onhorizontalSpeedChange(e)}
+                                    type="number"
+                                    min="-5"
+                                    max="5"
+                                    step="0.01"
+                                    value={horizontalSpeed}
                                 />
-                                Breaking News
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    value={false}
-                                    checked={NrcsBreakingText === false}
-                                    onChange={handleChange}
-                                />
-                                News Update
-                            </label></td>
-                        <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
-                            <button>Play</button>
-                            <button>Stop</button>
-                        </td>
-                    </tr>
+                            </td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
 
-                </tbody>
-            </table >
-        </div >
+                                <button onClick={() => {
+                                    playticker();
+
+                                }}>Play</button>
+                                {tickerRunning && (
+                                    <Timer
+                                        interval={3000}
+                                        callback={async () => {
+                                            // console.log(indexRefTicker.current)
+                                            const currentItem = scrollData[indexRefTicker.current];
+
+                                            if (currentItem) {
+                                                const aa = `SCENE "25IN_ChannelPackaging_351.450/vimlesh_ticker" Export "tScroll" SetValue "{'Group1':[{'vLeadingSpace':'0','vTrailingSpace':'${vTrailingSpace}','tText':'${currentItem}'}]}"`;
+
+                                                await fetch("/api/sendCommand", {
+                                                    method: "POST",
+                                                    headers: { "Content-Type": "application/json" },
+                                                    body: JSON.stringify({ command: aa }),
+                                                });
+                                            }
+
+                                            // update index safely
+                                            indexRefTicker.current = (indexRefTicker.current + 1) % scrollData.length;
+
+                                        }}
+                                    />
+                                )}
+                                <button onClick={onStopTicker}>Stop</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>Lower Third
+                                Breaking News</td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
+                                <button>Play</button>
+                                <button>Stop</button>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>  Full Pag
+                                Breaking News</td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
+                                <button>Play</button>
+                                <button>Stop</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}>News Update</td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
+                                <button>Play</button>
+                                <button>Stop</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style={{ border: '1px solid black', padding: '8px', fontWeight: 'bolder' }}> Two Liner
+                                Header:
+                                <br />
+                                Header: <br />
+                                <label>
+
+                                    <input
+                                        type="radio"
+                                        value={true}
+                                        checked={NrcsBreakingText === true}
+                                        onChange={handleChange}
+                                    />
+                                    Breaking News
+                                </label>
+                                <label>
+                                    <input
+                                        type="radio"
+                                        value={false}
+                                        checked={NrcsBreakingText === false}
+                                        onChange={handleChange}
+                                    />
+                                    News Update
+                                </label></td>
+                            <td style={{ border: '1px solid black', padding: '8px', textAlign: 'center' }}>
+                                <button>Play</button>
+                                <button>Stop</button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table >
+            </div >
+        </div>
+
+
+
 
     </>)
 }
