a091adbe27e27eb3494e010f24556a693fbd9694
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-09-22 16:51:17 +0530

    vimlesh_stream_test1

--- src/app/components/VideoPlayer.jsx
+++ src/app/components/VideoPlayer.jsx
@@ -4,6 +4,7 @@ const VideoPlayer = () => {
 
     const [file, setFile] = useState(null);
     const [htmlurl, sethtmlurl] = useState('https://google.co.in');
+    const [streamurl, setStreamurl] = useState('rtmp://45.116.3.113:1940/tata/livestream');
     const handleFileChange = (e) => {
         setFile(e.target.files[0]); // store the first selected file
     };
@@ -11,7 +12,9 @@ const VideoPlayer = () => {
     const handleFileChangetext = (e) => {
         sethtmlurl(e.target.value); // store the first selected file
     };
-
+    const handleStreamChange = (e) => {
+        setStreamurl(e.target.value); // store the first selected file
+    };
 
     const playFile = async () => {
         const exportValues = {
@@ -62,19 +65,62 @@ const VideoPlayer = () => {
     }
 
 
+    const stopStream = () => {
+        fetch("/api/timeline", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ project: '25IN_ChannelPackaging_351.450', scene: 'vimlesh_stream_test1', timeline: "Out" })
+        })
+    }
+
+    const playStream = async () => {
+        const exportValues = {
+            url1: streamurl,
+        }
+        fetch("/api/playwithexportedvalues", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                project: '25IN_ChannelPackaging_351.450',
+                scene: 'vimlesh_stream_test1',
+                timeline: 'In',
+                slot: "-1",
+                exportedvalues: Object.entries(exportValues).map(([name, value]) => ({ name, value }))
+            })
+        })
+    }
+
     return (<>
-        <div>
+        <div style={{ border: '1px solid red', padding: 10, margin: 10 }}>
+            <h1>
+                Video player
+            </h1>
             <h3>Open only from c:/casparcg/_media</h3>
             <input type="file" onChange={handleFileChange} />
             {file && <button onClick={playFile}>Play file</button>}
             <button onClick={stopFile}>Stop file</button>
         </div>
 
-        <div>
+        <div style={{ border: '1px solid red', padding: 10, margin: 10 }}>
 
+
+            <h1>
+                Html player
+            </h1>
             <input style={{ width: 800 }} type="text" value={htmlurl} onChange={handleFileChangetext} />
             <button onClick={playFilehtml}>Play file</button>
             <button onClick={stopFilehtml}>Stop file</button>
+        </div>
+
+        <div style={{ border: '1px solid red', padding: 10, margin: 10 }}>
+
+
+            <h1>
+                Stream player
+            </h1>
+            <input style={{ width: 800 }} type="text" value={streamurl} onChange={handleStreamChange} />
+            <button onClick={playStream}>Play file</button>
+            <button onClick={stopStream}>Stop file</button>
         </div>
     </>)
 }
