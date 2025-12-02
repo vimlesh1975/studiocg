192a55edbe33e882048de5bf9a4047590037bfca
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2025-12-02 12:23:37 +0530

    functions

--- src/app/components/unreal/Unreal.jsx
+++ src/app/components/unreal/Unreal.jsx
@@ -5,14 +5,15 @@ import Worldlocation from './Worldlocation';
 import Genesis from './Genesis';
 import ChromaKey from './ChromaKey';
 
-const instance = 3
+// const instance = 3
 // const editmode="UEDPIE_0_"
 // const editmode = ""
 
 const Unreal = () => {
     // const [editmode, setEditmode] = useState("UEDPIE_0_");
     const [editmode, setEditmode] = useState("");
-    const [editmodebool, setEditmodebool] = useState(false);
+    const [instance, setInstance] = useState(1);
+    // const [editmodebool, setEditmodebool] = useState(false);
     const [labels, setLabels] = useState(null);
 
     const [selectedValue, setSelectedValue] = useState('')
@@ -21,13 +22,7 @@ const Unreal = () => {
     const [selectedfunction, setSelectedfunction] = useState('');
 
     async function describe({ objectPath }) {
-        console.log(objectPath)
-        // setResult("Loading...");
-
         try {
-            // const objectPath =
-            //     "/Game/000_wTV_AR/Maps/COMPOSITING_LEVEL_LevelInstance_1.COMPOSITING_LEVEL:PersistentLevel.Cone_0";
-
             const res = await fetch(
                 `/api/unreal/remote/object/describe?objectPath=${encodeURIComponent(
                     objectPath
@@ -35,17 +30,13 @@ const Unreal = () => {
             );
 
             const json = await res.json();
-            // console.log(json.data.Functions);
             setFunctions(json.data.Functions)
-            // console.log((JSON.stringify(json)));
         } catch (err) {
-            // setResult("Error: " + err.message);
         }
     }
 
 
     function LabelCombo({ returnValue }) {
-        // Build options only once
         const options = useMemo(
             () =>
                 Object.entries(returnValue).map(([path, label]) => ({
@@ -55,23 +46,15 @@ const Unreal = () => {
             [returnValue]
         );
 
-        // // Default selected: first option whose label is "PLATE1"
-        // const defaultOption =
-        //     options.find(o => o.label === "PLATE1") || options[0];
-
-        // const [selectedValue, setSelectedValue] = useState(
-        //     defaultOption ? defaultOption.value : ""
-        // );
-
         return (<>
             <select
                 value={selectedValue}
                 onChange={(e) => {
                     setSelectedValue(e.target.value);
-                    // describe({ objectPath: "/Game/000_wTV_AR/Maps/UEDPIE_0_COMPOSITING_LEVEL_LevelInstance_1.COMPOSITING_LEVEL:PersistentLevel.Cone_0" })
-                    // describe({ objectPath: `/Game/000_wTV_AR/Maps/${editmode}COMPOSITING_LEVEL_LevelInstance_${instance}.COMPOSITING_LEVEL:PersistentLevel.Cone_0` })
-                    describe({ objectPath: '/Game/000_wTV_AR/Maps/' + editmode + (e.target.value).split('/Game/000_wTV_AR/Maps/')[1] })
-
+                    const input = e.target.value;
+                    const insert = editmode;
+                    const output = input.replace(/(\/maps\/)/i, `$1${insert}`);
+                    describe({ objectPath: output })
                 }}
             >
                 {options.map((opt, i) => (
@@ -80,21 +63,24 @@ const Unreal = () => {
                     </option>
                 ))}
             </select>
-            {selectedValue}
+            <div style={{ minWidth: 1200, backgroundColor: 'grey1' }}>
+                Path:  {selectedValue}
+            </div>
 
-            <select
-                value={selectedfunction}
-                onChange={(e) => {
-                    setSelectedfunction(e.target.value);
-                }}
-            >
-                {functions.map((opt, i) => (
-                    <option key={i} value={opt.Name}>
-                        {opt.Name}
-                    </option>
-                ))}
-            </select>
-            {/* {selectedfunction} */}
+            <div>
+                Function:  <select
+                    value={selectedfunction}
+                    onChange={(e) => {
+                        setSelectedfunction(e.target.value);
+                    }}
+                >
+                    {functions.map((opt, i) => (
+                        <option key={i} value={opt.Name}>
+                            {opt.Name}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
         </>);
     }
@@ -114,74 +100,52 @@ const Unreal = () => {
             // console.log(res)
             const json = await res.json();
             console.log(json.unreal.ReturnValue)
-            setEditmodebool(json.unreal.ReturnValue)
+            // setEditmodebool(json.unreal.ReturnValue)
             setEditmode(json.unreal.ReturnValue ? "UEDPIE_0_" : "")
 
             // setResult(JSON.stringify(json));
         } catch (err) {
             // setResult("Error: " + err.message);
         }
+
+        //get paths
+        try {
+            const res = await fetch("/api/unreal/remote/object/call", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    objectPath: "/Script/Wingman.Default__V3FL",
+                    functionName: "FetchV3PathLabels",
+                    Parameters: {},
+                    GenerateTransaction: true,
+                }),
+            });
+            const json = await res.json();
+            // console.log(json.unreal.ReturnValue)
+
+
+            const returnValue = json.unreal.ReturnValue;
+            setLabels(returnValue)
+
+            const aa = Object.entries(returnValue).map(([path, label]) => ({
+                value: path,   // what you send back to Unreal
+                label,        // what the user sees
+            }))
+            const str = aa[0].value;
+            const match = str.match(/_LevelInstance_(\d+)/);
+            const number = match ? match[1] : null;
+            setInstance(number)
+        } catch (err) {
+        }
     }
 
     return (
         <>
             <div>
-                {/* Edit Mode:   <input checked={editmodebool} type='checkbox' onChange={(e) => {
-                    setEditmodebool(val => !val);
-                    setEditmode(e.target.checked ? "UEDPIE_0_" : "")
-                }
-                } /> */}
-                {editmode}
-
-                {/* <button onClick={async () => {
-                    try {
-                        const res = await fetch("/api/unreal/remote/object/call", {
-                            method: "POST",
-                            headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify({
-                                objectPath: "/Script/Wingman.Default__V3FL",
-                                functionName: "IsEditorMode",
-                                Parameters: {},
-                                GenerateTransaction: true,
-                            }),
-                        });
-                        // console.log(res)
-                        const json = await res.json();
-                        console.log(json.unreal.ReturnValue)
-                        setEditmodebool(json.unreal.ReturnValue)
-                        setEditmode(json.unreal.ReturnValue ? "UEDPIE_0_" : "")
-
-                        // setResult(JSON.stringify(json));
-                    } catch (err) {
-                        // setResult("Error: " + err.message);
-                    }
-                }}>Get Mode</button> */}
                 <button onClick={initialise}>Initialise</button>
-
-                <button onClick={async () => {
-                    try {
-                        const res = await fetch("/api/unreal/remote/object/call", {
-                            method: "POST",
-                            headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify({
-                                objectPath: "/Script/Wingman.Default__V3FL",
-                                functionName: "FetchV3PathLabels",
-                                Parameters: {},
-                                GenerateTransaction: true,
-                            }),
-                        });
-                        const json = await res.json();
-                        // console.log(json.unreal.ReturnValue)
-
-                        const returnValue = json.unreal.ReturnValue;
-                        setLabels(returnValue)
-
-                    } catch (err) {
-                    }
-                }}>Get Labels and Paths</button>
-
-                {labels && < LabelCombo returnValue={labels} />}
-
+            </div>
+            <div>
+                Objects:  {labels && < LabelCombo returnValue={labels} />}
             </div>
             <div style={{ display: 'flex' }}>
                 <div style={{ border: '1px solid red' }}>
